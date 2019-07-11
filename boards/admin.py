ada12c2b0b006034ee0f70f3ca31798657ec78c7
sungwon <jsw2095@naver.com>
2019-07-12 03:17:47 +0900

    질의응답 모델 추가

--- boards/admin.py
+++ boards/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Board
+from .models import Board, Question
 
 
 @admin.register(Board)
-class PostAdmin(admin.ModelAdmin):
+class BoardAdmin(admin.ModelAdmin):
     list_diaply_links = (
         'title',
     )
@@ -17,6 +17,27 @@ class PostAdmin(admin.ModelAdmin):
 
     list_display = (
         'title',
+        'created_at',
+        'updated_at',
+    )
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_diaply_links = (
+        'title',
+    )
+
+    search_fields = (
+        'title',
+    )
+    
+    list_filter = (
+    )
+
+    list_display = (
+        'title',
+        'user',
         'created_at',
         'updated_at',
     )
--- boards/models.py
+++ boards/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from share.timestamp import TimeStampedModel
 from django.utils.translation import ugettext_lazy as _
+from users.models import User
 
 class Board(TimeStampedModel):
 
     class Meta:
-        verbose_name = '공지'
+        verbose_name = '공지사항'
         verbose_name_plural = "공지사항"
 
     title = models.CharField(_('제목'), max_length=200)
     content = models.TextField(_('내용'))
     view_count = models.IntegerField(_('조회수'), default=0)
+
+
+class Question(TimeStampedModel):
+
+    class Meta:
+        verbose_name = "질의응답"
+        verbose_name_plural = "질의응답"
+
+    user = models.ForeignKey(User, verbose_name=_('질문자'), on_delete=models.CASCADE)
+    title = models.CharField(_('제목'), max_length=200)
+    content = models.TextField(_('내용'))
