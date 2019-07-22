fced4eed93ea2dedb2bb374ed1e5ed031d92c6b7
= <=>
2019-07-22 16:33:11 +0000

    working

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,7 @@ from .views import sex, news
 urlpatterns = [
     path('sex/', sex, name="sex"),
     path('news/', news, name="news"),
+    path('b_content/', b_content, name="b_content"),
+    path('s_content/', s_content, name="s_content"),
+    path('n_content/',n_content, name="n_content"),
 ]
