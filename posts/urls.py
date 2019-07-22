22656d32b2f320a087bac6a258109b364d070e6e
= <=>
2019-07-22 18:15:21 +0000

    image card done

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import sex, news
+from .views import sex, news, show
 
 urlpatterns = [
     path('sex/', sex, name="sex"),
     path('news/', news, name="news"),
-    path('b_content/', b_content, name="b_content"),
-    path('s_content/', s_content, name="s_content"),
-    path('n_content/',n_content, name="n_content"),
+    path('show/<int:id>', show, name="show"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 import pdb
 
@@ -26,3 +26,21 @@ def news(request):
         'posts': posts
     }
     return render(request, 'posts/news.html', context)
+
+def show(request, id):
+    post = get_object_or_404(Post, pk=id)
+    return render(request,'posts/show.html', {'post':post})
+
+
+
+def create(request):
+    if request.method == "POST":
+        image = request.POST.get('image')
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        product = Product(image=image,title=title,content=content)
+        product.save()
+        return render(request,'posts/basic.html')
+
+    
+    
