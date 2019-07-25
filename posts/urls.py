65f4932e26f1600e3b90470370a4f7f802273689
sungwon <jsw2095@naver.com>
2019-07-25 22:04:04 +0900

    좋아요 기능 구현

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import sex, news, show
+from .views import sex, news, show, like_toggle
 
 urlpatterns = [
     path('sex/', sex, name="sex"),
     path('news/', news, name="news"),
-    path('show/<int:id>', show, name="show"),
+    path('show/<int:id>/', show, name="show"),
+    path('like_toggle/<int:post_id>/', like_toggle, name="like_toggle"),
 ]
